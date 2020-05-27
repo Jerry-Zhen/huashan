7e0f2143adf68703c971475ad7d1b3cbd2f70bce
jerry_glb <316308402@163.com>
2020-05-27 18:19:12 +0800

    new files

---
+++ src/main/java/com/jerry/git/App.java
@@ -0,0 +1,13 @@
+package com.jerry.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/jerry/git/Apple.java
@@ -0,0 +1,10 @@
+package com.jerry.git;
+
+/**
+ * @Description TODO
+ * @Author Jerry Chen
+ * @Date 2020/5/27
+ * @Version 1.0
+ */
+public class Apple {
+}
